b9c67c566eecb57e3e1cbebac37509a5f2dec433
khoi1908vn <khoimle7@gmail.com>
2023-01-22 14:50:21 +0700

    shit update

--- main.py
+++ main.py
@@ -27,52 +27,52 @@ class quotes:
 
     
 line_break = '\r\n'
-acceptall = [
-    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-    "Accept-Encoding: gzip, deflate\r\n",
-    "Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-    "Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n",
-    "Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
-    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
-    "Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
-    "Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-    "Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n,"
-    "Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n",
-    "Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-    "Accept: text/html, application/xhtml+xml",
-    "Accept-Language: en-US,en;q=0.5\r\n",
-    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n",
-    "Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
+class make_resources:
+    acceptall = [
+        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+        "Accept-Encoding: gzip, deflate\r\n",
+        "Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+        "Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n",
+        "Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
+        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
+        "Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
+        "Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+        "Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n,"
+        "Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n",
+        "Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+        "Accept: text/html, application/xhtml+xml",
+        "Accept-Language: en-US,en;q=0.5\r\n",
+        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n",
+        "Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
+        ]
+    referers = [
+        "https://www.google.com/search?q=",
+        "https://check-host.net/",
+        "https://www.facebook.com/",
+        "https://www.youtube.com/",
+        "https://www.fbi.com/",
+        "https://www.bing.com/search?q=",
+        "https://r.search.yahoo.com/",
+        "https://www.cia.gov/index.html",
+        "https://vk.com/profile.php?redirect=",
+        "https://www.usatoday.com/search/results?q=",
+        "https://help.baidu.com/searchResult?keywords=",
+        "https://steamcommunity.com/market/search?q=",
+        "https://www.ted.com/search?q=",
+        "https://play.google.com/store/search?q=",
+        "https://www.qwant.com/search?q=",
+        "https://soda.demo.socrata.com/resource/4tka-6guv.json?$q=",
+        "https://www.google.ad/search?q=",
+        "https://www.google.ae/search?q=",
+        "https://www.google.com.af/search?q=",
+        "https://www.google.com.ag/search?q=",
+        "https://www.google.com.ai/search?q=",
+        "https://www.google.al/search?q=",
+        "https://www.google.am/search?q=",
+        "https://www.google.co.ao/search?q=",
     ]
 
-referers = [
-	"https://www.google.com/search?q=",
-	"https://check-host.net/",
-	"https://www.facebook.com/",
-	"https://www.youtube.com/",
-	"https://www.fbi.com/",
-	"https://www.bing.com/search?q=",
-	"https://r.search.yahoo.com/",
-	"https://www.cia.gov/index.html",
-	"https://vk.com/profile.php?redirect=",
-	"https://www.usatoday.com/search/results?q=",
-	"https://help.baidu.com/searchResult?keywords=",
-	"https://steamcommunity.com/market/search?q=",
-	"https://www.ted.com/search?q=",
-	"https://play.google.com/store/search?q=",
-	"https://www.qwant.com/search?q=",
-	"https://soda.demo.socrata.com/resource/4tka-6guv.json?$q=",
-	"https://www.google.ad/search?q=",
-	"https://www.google.ae/search?q=",
-	"https://www.google.com.af/search?q=",
-	"https://www.google.com.ag/search?q=",
-	"https://www.google.com.ai/search?q=",
-	"https://www.google.al/search?q=",
-	"https://www.google.am/search?q=",
-	"https://www.google.co.ao/search?q=",
-]
-
-def closeProgram(reason: str = '\nExiting...'):
+def closeProgram(reason: str = '\n[~] Exiting...'):
     print(reason)
     exit()
 
@@ -171,8 +171,8 @@ def gen_user_agent():
 def make_header(target, path, cookies = ''):
     connection = "Connection: Keep-Alive" + line_break
     if cookies != '': connection += f"Cookies: {str(cookies)}" + line_break
-    accept = Choice(acceptall)
-    referer = Choice(referers)
+    accept = Choice(make_resources.acceptall)
+    referer = Choice(make_resources.referers)
     referer = f"Referer: {referer}" + target + path + line_break
     useragent = "User-Agent: " + gen_user_agent() + line_break
     return referer + useragent + accept + connection + line_break
@@ -274,7 +274,7 @@ def main():
     print('[~] Setting internal flag to False...')
     event.clear()
     print('[~] Set! Trying to close the program...')
-    closeProgram('[~] Finished')
+    closeProgram()
     for i in process_list:
         i.join()
     print('[~] Stopped')
